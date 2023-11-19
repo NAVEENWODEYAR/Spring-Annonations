f6709840866c887308ec3559e7a91360c3af04b8
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-19 10:59:28 +0530

    @Bean, custom logic to create beans manually

--- src/main/java/com/bhas/config/ConfigCls.java
+++ src/main/java/com/bhas/config/ConfigCls.java
@@ -1,5 +1,6 @@
 package com.bhas.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -8,5 +9,13 @@ public class ConfigCls
 	public ConfigCls()
 	{
 		System.out.println("\nConfigCls::Constructor");
+	}
+	
+	@Bean
+	public PasswordUtils getInstance()
+	{
+		System.out.println("\n getInstance() using @Bean!");
+		PasswordUtils pwd = new PasswordUtils("SHA-1");
+		return pwd;
 	}
 }
---
+++ src/main/java/com/bhas/config/PasswordUtils.java
@@ -0,0 +1,9 @@
+package com.bhas.config;
+
+public class PasswordUtils 
+{
+	public PasswordUtils(String algoritham)
+	{
+		System.out.println("\nPasswordUtils::Constructor");
+	}
+}
