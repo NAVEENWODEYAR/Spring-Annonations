a08a3ef4b21b5f56fb49339103fb65a296a1c9d0
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-19 10:14:41 +0530

    <context:annonation-config/>     <context:component-sacn base-package="com.bhas"/>

---
+++ src/main/java/com/bhas/utils/DateUtils2.java
@@ -0,0 +1,13 @@
+package com.bhas.utils;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DateUtils2 
+{
+	// Default constructor,
+	public DateUtils2()
+	{
+		System.out.println("DateUtils2::Constructor");
+	}
+}
